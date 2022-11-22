9e9680c7d9169d5712b1499166dc989313d372e5
Fanessa <fanessah@gmail.com>
2022-11-22 11:26:05 -0500

    api is working

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-// import express
+
 import express from 'express';
-//create a new server
+import cors from 'cors'
+import {MongoClient} from 'mongodb'
+import {uri} from "./secret.js"
+
+
+
 const app = express();
-// define port 
 const PORT = 3030;
+app.use(cors())
+app.use(express.json())
 
 
+const client = new MongoClient(uri)
+const db = client.db('overView')
+const posts = db.collection('reviews')
 
+
+client.connect()
+console.log('connected to MongoDB')
+
+// listen to the server on the specified port
 app.listen(PORT, () => {
     console.log (`Server is running on port ${PORT}`);
 });
 
 
 // CREATE MY ROUTES HERE 
+
+app.get('/',(req,res) => {
+    res.status(200).send("Hello World");
+})
+
+// ADD REVIEW 
+app.post('/',async (req,res) => {
+    const newReview = {author: 'Fanessa',date:'12-1-2022',program:'test bootcamp',review:[{overallExp:5},{curriculum:5},{instruction:5},{jobAssist:5}]}
+    console.log('req.body ->',req.body)
+
+    await posts.insertOne(newReview)
+    res.send('item was added')
+});
